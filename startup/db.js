6d8beacd5d0f80c7dabc147f0fe612b61d2d30cf
unknown <patiladi3119@gmail.com>
2024-01-09 13:21:21 +0530

    add mongo string

--- startup/db.js
+++ startup/db.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
-const winston = require("winston");
+const config = require("config");
 
 module.exports = function () {
   mongoose
-    .connect("mongodb://localhost/vidly")
+    .connect(config.get("db"))
     .then(() => console.log("Connected to MongoDB..."))
     .catch((err) => console.error("Could not connect to MongoDB...", err));
 };
+//"mongodb+srv://patiladi3119:AdityaPatil31@clusterreactproject.bzfqgnw.mongodb.net/?retryWrites=true&w=majority"
+//"mongodb+srv://patiladi3119:Adityapatil31@clusterreactproject.bzfqgnw.mongodb.net/?retryWrites=true&w=majority"
