dbd58278400d46e6a3e31452e811fa1efcaa86a8
unknown <patiladi3119@gmail.com>
2024-01-09 16:09:37 +0530

    files updated

--- startup/config.js
+++ startup/config.js
@@ -4,4 +4,10 @@ module.exports = function () {
   if (!config.get("jwtPrivateKey")) {
     throw new Error("FATAL ERROR: jwtPrivateKey is not defined.");
   }
+  if (!config.get("NODE_ENV")) {
+    throw new Error("FATAL ERROR: NODE_ENV is not defined.");
+  }
+  if (!config.get("db")) {
+    throw new Error("FATAL ERROR: db is not defined.");
+  }
 };
