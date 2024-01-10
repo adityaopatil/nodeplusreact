4982339ec9ebf0870142fa003d09ac3976655b25
unknown <patiladi3119@gmail.com>
2024-01-10 17:15:40 +0530

    updated .env

--- index.js
+++ index.js
@@ -12,5 +12,6 @@ require("./startup/config")();
 require("./startup/validation")();
 require("./startup/prod")(app);
 
+console.log(process.env);
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}....`));
