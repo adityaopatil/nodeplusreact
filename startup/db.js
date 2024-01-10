32f231733563f93951c03591d642eab209519b7a
unknown <patiladi3119@gmail.com>
2024-01-10 15:57:11 +0530

    added custom env

--- startup/db.js
+++ startup/db.js
@@ -2,8 +2,9 @@ const mongoose = require("mongoose");
 const config = require("config");
 
 module.exports = function () {
+  const db = config.get("db");
   mongoose
-    .connect(config.get("db"))
-    .then(() => console.log("Connected to MongoDB..."))
+    .connect(db)
+    .then(() => console.log(`Connected to ${db}...`))
     .catch((err) => console.error("Could not connect to MongoDB...", err));
 };
