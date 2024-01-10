15d95d81924368ecbbcdcdc7d0813d3285cb7924
unknown <patiladi3119@gmail.com>
2024-01-10 21:49:34 +0530

    fixes

--- startup/config.js
+++ startup/config.js
@@ -2,10 +2,10 @@ const config = require("config");
 require("dotenv").config();
 
 module.exports = function () {
-  if (!config.get("jwtPrivateKey")) {
+  if (!process.env.vidly_jwtPrivateKey && !config.get("jwtPrivateKey")) {
     throw new Error("FATAL ERROR: jwtPrivateKey is not defined.");
   }
-  if (!config.get("db")) {
+  if (!process.env.vidly_db && !config.get("db")) {
     throw new Error("FATAL ERROR: db is not defined.");
   }
 };
